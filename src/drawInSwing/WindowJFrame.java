83b11d0d39b788da1c8ba51669ea916dc59975cb
LaunDan <launadaniel2@gmail.com>
2020-03-27 20:32:23 +0100

    add mypanel to the main frame

--- src/drawInSwing/WindowJFrame.java
+++ src/drawInSwing/WindowJFrame.java
@@ -7,6 +7,9 @@ public class WindowJFrame extends JFrame {
     public WindowJFrame(){
         this.setTitle("Main window of program");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        MyPanel panel = new MyPanel();
+        this.add(panel);
         this.setSize(400, 300);
         this.setLocationRelativeTo(null);
     }
