978fa8b2c737b3df460a6a9b0aac4ce217ff05ec
LaunDan <launadaniel2@gmail.com>
2020-03-28 20:23:36 +0100

    set colors

--- src/drawInSwing/MyPanel.java
+++ src/drawInSwing/MyPanel.java
@@ -7,6 +7,18 @@ public class MyPanel extends JPanel {
 
     public MyPanel(){
         this.setBackground(Color.BLACK);
+        this.setFont(new Font("Courier", Font.BOLD, 20));
         this.setPreferredSize(new Dimension(400,300));
     }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        g.drawString("Hi", 50, 80);
+        g.setColor(Color.GREEN);
+        g.drawString("Hello", 120, 70);
+        g.setColor(Color.WHITE);
+        g.drawString("Good morning", 10, 210);
+    }
+
 }
